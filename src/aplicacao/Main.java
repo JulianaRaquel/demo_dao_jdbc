ff14af9177b2d1a52327f81257ddb6617131826f
juliana_21 <julianamedeiros.dev@gmail.com>
2025-06-22 07:39:12 -0300

    adicionados pacotes de entidades e aplicacao2

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import entities.Departamento;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Departamento dep = new Departamento(10, "Livros");
 
 	}
 
--- src/entities/Departamento.java
+++ src/entities/Departamento.java
@@ -1,5 +1,36 @@
 package entities;
 
 public class Departamento {
+	private int id;
+	private String nome;
+	
+	public Departamento() {}
+	
+	public Departamento(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + "]";
+	}
+	
+	
 }
